7891a90ed603fa83dce670266f6e9afcc2cc4d25
David Haley <davidhaley@protonmail.com>
2017-04-22 11:50:51 -0600

    feat: Create spinning animation for fan blades

--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Fan : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject fanBlades;
+
+    private float speed = 4f;
+
+    private void Update()
+    {
+        // One 360 degree rotation per second, muliplied by speed (ie. 4 means 4 360 degree rotations per second)
+        fanBlades.transform.Rotate(Vector3.forward, (Time.deltaTime * 360) * speed);
+    }
 }
